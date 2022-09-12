bf5a3423f0e42f363dd0005ae2276712a823a74a
ggiande <44848977+ggiande@users.noreply.github.com>
2022-09-12 15:17:34 -0700

    fixed navbar light mode and anchoring, resources for grad req moved

--- script/index.js
+++ script/index.js
@@ -4,10 +4,10 @@ const header = document.querySelector(".navbar");
 
 // TODO: Rewrite for when sticky is active
 window.onscroll = function () {
-	var top = window.scrollY;
-	if (top >= 400) {
-		header.classList.add("bg-light", "rounded");
-	} else {
-		header.classList.remove("bg-light", "rounded");
-	}
+  var top = window.scrollY;
+  if (top >= 400) {
+    header.classList.add("bg-light");
+  } else {
+    header.classList.remove("bg-light");
+  }
 };
