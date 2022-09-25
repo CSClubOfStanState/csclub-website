e78cb619afe6a2602977dedf9e7ef94b17493a2c
Satyam Raghuwanshi <satyamraghuwanshi136@gmail.com>
2022-09-25 17:30:30 +0530

    added single nav bar functionality

---
+++ script/navbar.js
@@ -0,0 +1,56 @@
+class Navbar extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <nav
+        class="navbar sticky-top navbar-expand-lg"
+        aria-label="Tenth navbar example"
+      >
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarsExample08"
+            aria-controls="navbarsExample08"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div
+            class="collapse navbar-collapse justify-content-md-center"
+            id="navbarsExample08"
+          >
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">
+                  <i class="fa-solid fa-turn-up fa-xl"></i>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./index.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./index.html#Blog">Blog</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="./index.html#Resources">Resources</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="./officers.html#">Officers</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./archives/archive.html#">Archive</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    `;
+  }
+}
+    
+customElements.define('my-navbar', Navbar);
