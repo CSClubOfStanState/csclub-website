4f2dd95afe2e836027495f2ab82bfb1da61ea146
Giancarlo Garcia Deleon <44848977+ggiande@users.noreply.github.com>
2022-10-06 06:23:38 -0700

    Development (#35)
    * changed css for mobile col
    
    * ggiande/oss prep (#6)
    
    * first commit of oss branch
    
    * added code of conduct, license, readme, contributing files
    
    * Ggiande/oss blogpost (#17)
    
    * used cited authors, began oss blog post
    
    Signed-off-by: ggiande <44848977+ggiande@users.noreply.github.com>
    
    * completed oss post
    
    * added readme instructions for first contribution
    
    Signed-off-by: ggiande <44848977+ggiande@users.noreply.github.com>
    
    Signed-off-by: ggiande <44848977+ggiande@users.noreply.github.com>
    
    * reformatted root pages and fixed the links (#23)
    
    * Development (#21)
    
    * changed css for mobile col
    
    * ggiande/oss prep (#6)
    
    * first commit of oss branch
    
    * added code of conduct, license, readme, contributing files
    
    * Ggiande/oss blogpost (#17)
    
    * used cited authors, began oss blog post
    
    Signed-off-by: ggiande <44848977+ggiande@users.noreply.github.com>
    
    * completed oss post
    
    * added readme instructions for first contribution
    
    Signed-off-by: ggiande <44848977+ggiande@users.noreply.github.com>
    
    Signed-off-by: ggiande <44848977+ggiande@users.noreply.github.com>
    
    Signed-off-by: ggiande <44848977+ggiande@users.noreply.github.com>
    
    * reformatted root pages and fixed the links
    
    Signed-off-by: ggiande <44848977+ggiande@users.noreply.github.com>
    Co-authored-by: Giancarlo Garcia Deleon <44848977+ggiande@users.noreply.github.com>
    
    * Added single navigation bar functionality.  (#20)
    
    * added single nav bar functionality
    
    * added single nav bar functionality
    
    * Development (#21)
    
    * changed css for mobile col
    
    * ggiande/oss prep (#6)
    
    * first commit of oss branch
    
    * added code of conduct, license, readme, contributing files
    
    * Ggiande/oss blogpost (#17)
    
    * used cited authors, began oss blog post
    
    Signed-off-by: ggiande <44848977+ggiande@users.noreply.github.com>
    
    * completed oss post
    
    * added readme instructions for first contribution
    
    Signed-off-by: ggiande <44848977+ggiande@users.noreply.github.com>
    
    Signed-off-by: ggiande <44848977+ggiande@users.noreply.github.com>
    
    Signed-off-by: ggiande <44848977+ggiande@users.noreply.github.com>
    
    Signed-off-by: ggiande <44848977+ggiande@users.noreply.github.com>
    Co-authored-by: Giancarlo Garcia Deleon <44848977+ggiande@users.noreply.github.com>
    
    * Ggiande/26 version 2 new features (#28)
    
    * added events page, added to navbar
    
    Signed-off-by: ggiande <44848977+ggiande@users.noreply.github.com>
    
    * quick fixes for linkrels, images and logos
    
    * removed emojis
    
    Signed-off-by: ggiande <44848977+ggiande@users.noreply.github.com>
    
    * add logo for arhive.html
    
    * merged conflicts and added lost commits
    
    Signed-off-by: ggiande <44848977+ggiande@users.noreply.github.com>
    
    * created dynamic footer (#34)
    
    * Removed contact.html page (#33)
    
    Signed-off-by: ggiande <44848977+ggiande@users.noreply.github.com>
    Co-authored-by: satyamraghuwanshi136 <90232249+satyamraghuwanshi136@users.noreply.github.com>

---
+++ script/footer.js
@@ -0,0 +1,68 @@
+class Footer extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <div class="container">
+			<footer class="py-3 my-4">
+				<ul class="nav justify-content-center border-bottom pb-2 mb-2">
+					<li class="nav-item">
+						<a href="./index.html" class="nav-link px-2 text-muted">Home</a>
+					</li>
+				
+					<li class="nav-item">
+						<a href="./index.html#Blog" class="nav-link px-2 text-muted"
+							>Blog</a
+						>
+					</li>
+					<li class="nav-item">
+						<a href="./index.html#Resources" class="nav-link px-2 text-muted"
+							>Resources</a
+						>
+					</li>
+          <li class="nav-item">
+            <a href="./officers.html" class="nav-link px-2 text-muted"
+              >Officers</a
+            >
+          </li>
+					<li class="nav-item">
+						<a
+							class="nav-link px-2 text-muted"
+							href="./archive.html#"
+							>Archive</a
+						>
+					</li>
+          <li class="nav-item">
+						<a
+							class="nav-link px-2 text-muted"
+							href="./events.html#"
+							>Events</a
+						>
+					</li>
+					<li class="nav-item">
+						<a
+							href="https://www.instagram.com/csclubofcsustan/"
+							target="_blank"
+							class="nav-link px-2 text-muted"
+							>Instagram</a
+						>
+					</li>
+					<li class="nav-item">
+						<a
+							href="https://discord.com/invite/Gp5V58B"
+							target="_blank"
+							class="nav-link px-2 text-muted"
+							>Discord</a
+						>
+					</li>
+				</ul>
+				<p class="text-center text-muted">
+					© <span id="yearDate"></span>
+					2022 Computer Science Club at California State University,
+					Stanislaus
+				</p>
+			</footer>
+		</div>
+    `;
+  }
+}
+
+customElements.define("my-footer", Footer);
