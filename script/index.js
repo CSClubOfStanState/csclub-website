47c78c3470370b11f4a3afda0f62f3fcbb39c4da
Giancarlo Garcia Deleon <44848977+ggiande@users.noreply.github.com>
2022-10-12 22:25:57 -0700

    changed navbar to be at the top, changed opacity (#38)

--- script/index.js
+++ script/index.js
@@ -1,17 +1,5 @@
 let yearDate = new Date().getFullYear();
 
-const header = document.querySelector(".navbar");
-
-// TODO: Rewrite for when sticky is active
-window.onscroll = function () {
-  var top = window.scrollY;
-  if (top >= 400) {
-    header.classList.add("bg-light");
-  } else {
-    header.classList.remove("bg-light");
-  }
-};
-
 // Tool Tips Init Objects
 const tooltipTriggerList = document.querySelectorAll(
   '[data-bs-toggle="tooltip"]'
