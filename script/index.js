a5ebb6dfeca1f3410bb17143b01db3252ce978f4
Giancarlo Garcia Deleon <44848977+ggiande@users.noreply.github.com>
2022-10-19 16:37:32 -0700

    41 add unix section under resources (#42)
    * created wip unix page
    
    * added modal, wip resources page for unix
    
    * added all headings to cover for the unix page
    
    * removed Giancarlo Garcia Deleon as an officer
    
    * completed unix page content
    
    * fixed sign up button

--- script/index.js
+++ script/index.js
@@ -7,3 +7,13 @@ const tooltipTriggerList = document.querySelectorAll(
 const tooltipList = [...tooltipTriggerList].map(
   (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
+
+const myModal = document.getElementById("myModal");
+const myInput = document.getElementById("myInput");
+
+myModal.addEventListener("shown.bs.modal", () => {
+  myInput.focus();
+});
+$(document).ready(function () {
+  $("#MyModal").modal();
+});
